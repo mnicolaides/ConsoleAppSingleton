794a535953d1740448c6e1829adad3bf35554dc6
Martin W Nicolaides <martinnicolaides@outlook.com>
2020-02-11 09:32:19 -0500

    Updated

--- ConsoleAppSingleton/Program.cs
+++ ConsoleAppSingleton/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleAppSingleton
 {
@@ -6,30 +8,45 @@ namespace ConsoleAppSingleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Using a parallel for loop to slam the singleton instance almost simultaneously
+            Parallel.For(0, 100, i =>
+            {
+                Singleton.Instance.Write(i.ToString());
+            });
         }
     }
 
     public sealed class Singleton
     {
+        // Called first
         private static readonly Singleton instance = new Singleton();
 
         // Explicit static constructor to tell C# compiler
-        // not to mark type as beforefieldinit
+        // not to mark type as beforefieldinit.
+        // Called third
         static Singleton()
         {
         }
 
+        // Called second
         private Singleton()
         {
+            // If called more than once multiple timestamps will display.
+            Console.WriteLine($"[{DateTime.UtcNow.ToString("HH:MM:ss.ffffff")}]");
         }
 
+        // Called fourth
         public static Singleton Instance
         {
             get
             {
                 return instance;
             }
+        }
+
+        public void Write(string param)
+        {
+            Console.WriteLine($"[{param}]");
         }
     }
 }
