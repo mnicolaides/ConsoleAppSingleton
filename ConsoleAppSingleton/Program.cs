5c91cb6c250229b0d5bfb859793d983d47f50f0c
Martin W Nicolaides <martinnicolaides@outlook.com>
2020-02-11 08:19:46 -0500

    Add project files.

---
+++ ConsoleAppSingleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
